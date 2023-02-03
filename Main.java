f3d59b34f615bda861d20c057b06321616cc630c
FTigashov <tigashov.filipp@yandex.ru>
2023-02-03 13:40:09 +0300

    some lil changes

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
-class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello world!\n");
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.print("Test\n");
     }
 }
